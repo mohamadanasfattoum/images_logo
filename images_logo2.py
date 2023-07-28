ecc4aa1491e41c57005a228bfa6ef0423236e2bf
Mohamad anas fattoum <samaanas30@gmail.com>
2023-07-29 00:12:05 +0200

    add-images_logo-add-Logos

--- images_logo2.py
+++ images_logo2.py
@@ -1,10 +1,11 @@
 from PIL import Image
 import os
+os.chdir('.')
 
 output_folder = input('Enter output_folder Name: ')
 logo_name = input('Enter logo_name Name: ')
 
-os.chdir('images')
+
 
 
 if not os.path.isdir(output_folder):  # python create folder if not exists
@@ -14,13 +15,12 @@ logo = Image.open(logo_name)
 logo_width , logo_height = logo.size
 
 
-for filename in os.listdir('.'):    #python code loop in folder file
-    #print(filename)
+for filename in os.listdir('.'):
     if filename.endswith(('.jpg','.png','.jp')):
         image = Image.open(filename)
         width , heigth = image.size
 
-        image.paste(logo,(width - logo_width , heigth - logo_heigth))
+        image.paste(logo,(width - logo_width , heigth - logo_height))
         image.save(os.path.join(output_folder,filename.lower()))
 
 print('FINISHED...')        
